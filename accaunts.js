da8891cacc2f81d83f3e190ac1c2b35b1cd6990d
ownspaceegorova <own.space.egorova@yandex.ru>
2022-12-19 21:53:48 +0500

    переключение таблиц внутри экрана

--- accaunts.js
+++ accaunts.js
@@ -40,10 +40,57 @@ for (i = 0; trashIcon.length > i; i++) {
 let penIcon = document.getElementsByClassName("pen-icon");
 for (i = 0; penIcon.length > i; i++) {
   penIcon[i].onclick = function() {
-    console.log('click');
     let popupSet = document.getElementById('popup-set');
     popupSet.classList.toggle('show-popup-set');
 }
+let popupCloseSet =  document.getElementById ("button-cansel-set");
+popupCloseSet.onclick = function  () {
+let popupSet = document.getElementById('popup-set');
+popupSet.classList.toggle("show-popup-set");
+}
 }
 
+let changePopup = document.getElementById('popup-set-text');
+changePopup.onclick = function () {
+    let popupSet = document.getElementById('popup-set');
+    popupSet.classList.toggle("show-popup-set");
+    let setFio = document.getElementById('popup-set-fio');
+    setFio.classList.toggle('show-popup-set-fio');
+}
 
+let popupCloseSetFio =  document.getElementById ("button-cansel-set-fio");
+    popupCloseSetFio.onclick = function  () {
+    let setFio = document.getElementById('popup-set-fio');
+    setFio.classList.toggle("show-popup-set-fio");
+}
+
+function openTable(evt, tableName) {
+ 
+  var i, tableScreen, tablinks;
+  tableScreen = document.getElementsByClassName("table-screen");
+  for (i = 0; i < tableScreen.length; i++) {
+    tableScreen[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("but-active", "");
+  }
+
+  document.getElementById(tableName).style.display = "block";
+  evt.currentTarget.className += "but-active";
+}
+
+let penIconTable2 = document.getElementsByClassName("pen-icon-table2");
+for (i = 0; penIconTable2.length > i; i++) {
+  penIconTable2[i].onclick = function() {
+    console.log('click')
+  let popupSetCity = document.getElementById('popup-set-city');
+  popupSetCity.classList.toggle('show-popup-set-city');
+}
+let popupCloseCity =  document.getElementById ("button-cansel-city");
+popupCloseCity.onclick = function  () {
+let popupSetCity = document.getElementById('popup-set-city');
+popupSetCity.classList.toggle("show-popup-set-city");
+}
+}
